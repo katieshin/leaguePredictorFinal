7ec17120dde0e614c50668d9d6e96a0543749473
Katie Shin <kshin@dhcp-10-105-98-79.wireless.northwestern.private>
2017-05-17 22:50:05 -0500

    apigetter_2.js

---
+++ riotAPI/apigetter_2.js
@@ -0,0 +1,32 @@
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+const KEY = 'RGAPI-80ac7c32-a460-494e-80d4-7f2e7c394535';
+
+const RiotAPI = function() {
+	this.BASE_PATH = 'https://na1.api.riotgames.com';
+
+	this.getAccountID = function(summonerName) {
+		let req = new XMLHttpRequest();
+		let url = this.BASE_PATH
+			      + '/lol/summoner/v3/summoners/by-name/'
+				  + summonerName + '?api_key=' + KEY;
+
+		//req.open('GET', url, true);
+		req.onreadystatechange = function() {
+			console.log('readyState', req.readyState);
+			if (req.readyState == 4 && req.status == 200) {
+				let result = JSON.parse(req.responseText);
+				console.log('result', result)
+				return result['accountId'];
+			}
+		}
+		req.open('GET', url, true);
+		req.send();
+	};
+};
+
+
+
+let r = new RiotAPI();
+
+var accountId = r.getAccountID('sonataine');
+console.log('accountId', accountId);
