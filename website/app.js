3e94867f44eac8a150612c6203da6d0801445816
edysuh <edys@u.northwestern.edu>
2017-06-04 18:14:04 -0500

    port...

--- website/app.js
+++ website/app.js
@@ -2,9 +2,11 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+app.set('port', process.env.port);
+
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 // app.listen(9000, () => {
-app.listen(process.env.port, () => {
+app.listen(app.get('port'), () => {
 	console.log("lol predictor static site hosted on port 9000");
 });
